7989a60254bb80224eb7bb660050d85e85874345
artace23 <artace23@gmail.com>
2025-06-09 09:58:34 +0800

    Changes of the video player for better streaming

--- src/app/watch/[imdb_id]/page.tsx
+++ src/app/watch/[imdb_id]/page.tsx
@@ -6,10 +6,9 @@ export default async function WatchPage({ params }: { params: Promise<{ imdb_id:
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      
       <div className="aspect-w-16 aspect-h-9 w-full rounded overflow-hidden mb-4">
         <iframe
-          src={`https://vidsrc.icu/embed/movie/${imdb_id}`}
+          src={`https://vidsrc.xyz/embed/movie/${imdb_id}`}
           allow="autoplay; fullscreen"
           scrolling="no"
           className="block w-full h-full min-h-[400px] bg-black"
